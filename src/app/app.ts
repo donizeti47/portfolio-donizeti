ca4868bf0e2e0ade44b42566b5d237b05d17eeb2
Donizeti Souza <donizeti@plannertome.com>
2026-09-09 00:09:47 -0300

    P2M 504 - feat(about): implementa secao sobre mim com imagem dinamica por tema e metricas

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header';
 import { HeroComponent } from './components/hero/hero';
+import { AboutComponent } from './components/about/about';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, HeroComponent],
+  imports: [HeaderComponent, HeroComponent, AboutComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
