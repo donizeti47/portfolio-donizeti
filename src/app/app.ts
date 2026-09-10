68ca2088695b1430c0005820531ba9a3f632cbf3
Donizeti Souza <donizeti@plannertome.com>
2026-09-10 17:55:19 -0300

    [P2M 505] - feat(skills): implement technical stack section

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header';
 import { HeroComponent } from './components/hero/hero';
 import { AboutComponent } from './components/about/about';
+import { SkillsComponent } from './components/skills/skills';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, HeroComponent, AboutComponent],
+  imports: [HeaderComponent, HeroComponent, AboutComponent, SkillsComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
